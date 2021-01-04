cf15007a9c6dc506a35eaa5282afd0318e26dab3
liu <7742600@qq.com>
2021-01-04 22:58:17 +0800

    test1

---
+++ src/main/java/com/shangma/cn/controller/JavaTest.java
@@ -0,0 +1,13 @@
+package com.shangma.cn.controller;
+
+/**
+ * @author 刘士萱
+ * @time 2021/1/4 - 22:54
+ */
+public class JavaTest {
+
+    public static void main(String[] args) {
+        System.out.println("AAAAA");
+    }
+
+}
